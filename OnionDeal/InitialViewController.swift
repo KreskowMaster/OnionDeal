7695883476a7fd4350847a8571261139116ccea2
Krystian Gontarek <krystian@gontarek.net>
2016-04-08 22:18:27 +0200

    Initial Commit

---
+++ OnionDeal/InitialViewController.swift
@@ -0,0 +1,19 @@
+//
+//  InitialViewController.swift
+//  OnionDeal
+//
+//  Created by Krystian Gontarek on 08.04.2016.
+//  Copyright © 2016 OnionDealDevs. All rights reserved.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ OnionDealTests/OnionDealTests.swift
@@ -0,0 +1,36 @@
+//
+//  OnionDealTests.swift
+//  OnionDealTests
+//
+//  Created by Krystian Gontarek on 08.04.2016.
+//  Copyright © 2016 OnionDealDevs. All rights reserved.
+//
+
+import XCTest
+@testable import OnionDeal
+
+class OnionDealTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
