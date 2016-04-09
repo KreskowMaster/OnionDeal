db91f52dd4c488d256993fa40f50c65c94e34b7b
Thorsten Klusemann <tklusemann.dev@gmail.com>
2016-04-09 20:03:06 +0200

    Created empty ViewControllers for the management screens

---
+++ OnionDeal/ProductsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ProductsViewController.swift
+//  OnionDeal
+//
+//  Created by Thorsten Klusemann on 09.04.16.
+//  Copyright © 2016 OnionDealDevs. All rights reserved.
+//
+
+import UIKit
+
+class ProductsViewController : UIViewController {
+    
+}
