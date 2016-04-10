bc037c5e86340b7a23380e169771a12c2a1cc755
Thorsten Klusemann <tklusemann.dev@gmail.com>
2016-04-10 12:19:07 +0200

    Connected outlets and actions in EditProductViewController

--- OnionDeal/EditProductViewController.swift
+++ OnionDeal/EditProductViewController.swift
@@ -9,5 +9,16 @@
 import UIKit
 
 class EditProductViewController : UITableViewController {
+    @IBOutlet weak var productNameTextField: UITextField!
+    @IBOutlet weak var basePriceTextField: UITextField!
+    @IBOutlet weak var afterPriceTextField: UITextField!
+    @IBOutlet weak var discountLabel: UILabel!
+    @IBOutlet weak var expireDateLabel: UILabel!
+    @IBOutlet weak var setExpireDateButton: UIButton!
+    @IBOutlet weak var addPhotoButton: UIButton!
     
+    @IBAction func addPhotoButtonPressed(sender: UIButton) {
+    }
+    @IBAction func setExpireDatePressed(sender: UIButton) {
+    }
 }
